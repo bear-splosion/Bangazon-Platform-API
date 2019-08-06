38319a02f9e78bcf58e067b05830fe25916b2600
Josh Webb <joshwebb42@gmail.com>
2019-08-06 11:03:06 -0500

    new masterrrr

--- BangazonAPI/Models/ComputerEmployee.cs
+++ BangazonAPI/Models/ComputerEmployee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace BangazonAPI.Models
 {
     public class ComputerEmployee
     {
+        public int Id { get; set; }
+        [Required]
+        public int ComputerId { get; set; }
+        [Required]
+        public int EmployeeId { get; set; }
+        [Required]
+        public DateTime AssignDate { get; set; }
+        public DateTime UnassignDate { get; set; }
+
     }
 }
