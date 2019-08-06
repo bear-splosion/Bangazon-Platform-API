f02fc7b5c6665ca188aa6b7e06cc26158dce3b4e
Matthew Abebe <matthewabebe.inbox@gmail.com>
2019-08-06 15:36:34 -0500

    working on controller

---
+++ BangazonAPI/Controllers/ProductTypeController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductTypeController : ControllerBase
+    {
+
+        private readonly IConfiguration _config;
+
+        public ProductTypeController(IConfiguration config)
+
+        { _config = config; }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+
+            }
+        }
+
+
+        // GET: api/<controller>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    string SqlCommandText = @"SELECT p.Id, p.[Name]
+                                            FROM ProductType p
+                                            WHERE 1 = 1";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<ProductType> productTypes = new List<ProductType>();
+                    while (reader.Read())
+                    {
+                        ProductType productType = new ProductType
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name"))
+                        };
+
+                        productTypes.Add(productType);
+
+                    }
+
+                    reader.Close();
+
+                    return Ok(productTypes);
+                }
+            }
+    }
+
+    // GET api/<controller>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/<controller>
+    [HttpPost]
+    public void Post([FromBody]string value)
+    {
+    }
+
+    // PUT api/<controller>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody]string value)
+    {
+    }
+
+    // DELETE api/<controller>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
+}
---
+++ TestBangazonAPI/TestProductType.cs
@@ -0,0 +1,39 @@
+﻿using BangazonAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestBangazonAPI
+{
+    public class TestProductType
+    {
+        [Fact]
+        public async Task Test_Get_All_ProductTypes()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                 * Arrange
+                 */
+
+                /*
+                 * Act
+                 */
+                var response = await client.GetAsync("/api/producttypes");
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var productTypes = JsonConvert.DeserializeObject<List<ProductType>>(responseBody);
+
+                /*
+                 * ASSERT
+                 */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(productTypes.Count > 0);
+            }
+        }
+    }
+}
