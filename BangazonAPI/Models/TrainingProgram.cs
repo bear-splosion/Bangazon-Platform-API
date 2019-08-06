cd8536fea7cd885ae691ded9bd81e74513d99220
Matthew Abebe <matthewabebe.inbox@gmail.com>
2019-08-06 11:32:54 -0500

    models created

--- BangazonAPI/Models/TrainingProgram.cs
+++ BangazonAPI/Models/TrainingProgram.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BangazonAPI.Models
 {
-    public class Class1
+    public class TrainingProgram
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string StartDate { get; set; }
+
+        [Required]
+        public string EndDate { get; set; }
+
+        [Required]
+        public string MaxAttendants { get; set; }
+
     }
 }
