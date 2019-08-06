71857fb3780a46fe1609e2eb3f2f9da4740af3f5
Keisha <49162665+keishacard@users.noreply.github.com>
2019-08-06 13:13:51 -0500

    fixed orderid

--- BangazonAPI/Models/OrderProduct.cs
+++ BangazonAPI/Models/OrderProduct.cs
@@ -8,7 +8,7 @@ namespace BangazonAPI.Models
         public int Id { get; set; }
 
         [Required]
-        public int OrderIdId { get; set; }
+        public int OrderId { get; set; }
 
         [Required]
         public int ProductId { get; set; }
